2e0316a2246f1d41afadc22ac978e56852c862db
Noah Husby <noahhusby@gmail.com>
2017-12-11 15:10:32 -0500

    Fixed I2C communication to displays, added support for 8-wheel drive.

--- src/main/java/org/usfirst/frc/team1701/robot/subsystems/I2CBus.java
+++ src/main/java/org/usfirst/frc/team1701/robot/subsystems/I2CBus.java
@@ -4,13 +4,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team1701.robot.RobotMap;
 import edu.wpi.first.wpilibj.I2C;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class I2CBus extends Subsystem {
+
+
 
     private final I2C i2cCommunication = RobotMap.arduinoCommunication;
 
     public I2C getI2cCommunication() {
         return i2cCommunication;
     }
+
+    public void sendTo(int device, int value)
+    {
+        i2cCommunication.write(device, value);
+    }
+
 
     public void communicate(int message)
     {
@@ -19,6 +31,14 @@ public class I2CBus extends Subsystem {
          * Refer to https://goo.gl/Pk1HKm for I2C commands.
          */
         i2cCommunication.write(RobotMap.arduinoInterface, message);
+    }
+
+    public void sendToDisplay(int toSend, int display)
+    {
+        for (int i=0; i<4; i++)
+        {
+            i2cCommunication.write(display, toSend);
+        }
     }
 
     protected void initDefaultCommand() {
